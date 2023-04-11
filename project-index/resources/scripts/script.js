d027484e3c4f0916b38543f01313de2e40dcf4cf
Matt <mhgarry92@gmail.com>
2023-04-11 01:36:27 -0400

    showing current weather removed filler to show future forecast

--- project-index/resources/scripts/script.js
+++ project-index/resources/scripts/script.js
@@ -28,7 +28,23 @@ $(document).ready(function() {
     });
   }
 
+  // Function to display current weather data
+  function displayCurrentWeather(data) {
 
+		//display city name, date, and,
+	$("#city-name")[0].textContent ="City: " + data.city.name + " " + dayjs().format(' MM/DD:mm:ss/YYYY')
+
+
+		$("#icon")[0].src = "http://openweathermap.org/img/wn/" + data.list[0].weather[0].icon + "@2x.png";
+		$("#temp")[0].textContent = "Temperature: " + data.list[0].main.temp + " Fahrenheit";
+		$("#humidity")[0].textContent = "Humidity: " + data.list[0].main.humidity + "%";
+		$("#wind")[0].textContent = "Wind speed " + data.list[0].wind.speed;
+		}
+
+  // Function to display weather forecast data
+  function displayForecast(data) {
+
+  }
 
   // Event listener for search button
   $("#submit").on("click", function(event) {
@@ -37,6 +53,11 @@ $(document).ready(function() {
     getWeather(city);
   });
 
-
+  // Event listener for search history dropdown
+  $(".storage").on("click", ".dropdown-item", function(event) {
+    event.preventDefault();
+    var city = $(this).text();
+    getWeather(city);
+  });
 
 });
